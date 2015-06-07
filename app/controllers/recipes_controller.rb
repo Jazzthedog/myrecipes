4af932ab6f88de4d8b0e9a7c4788029f6cf55ddf
jazzthedog <jazzthedog.bowwow@gmail.com>
2015-06-07 23:36:36 +0000

    added like/dislike counts and validations and sorting

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController  < ApplicationController
  
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.all.sort_by{|likes| likes.thumbs_up_total}.reverse
   end
   
   def show
@@ -43,9 +43,14 @@ class RecipesController  < ApplicationController
   
   def like
     @recipe = Recipe.find(params[:id])
-    Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
-    flash[:success] = "You recipe was updated successfully"
-    redirect_to :back
+    like = Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+    if like.valid?
+      flash[:success] = "You recipe was updated successfully"
+      redirect_to :back
+    else
+      flash[:danger] = "You can only like/dislike a recipe once! Sorry."
+      redirect_to :back    
+    end
   end
 
   
