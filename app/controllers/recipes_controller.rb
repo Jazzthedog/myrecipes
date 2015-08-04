50a41e15688613f82b1cdd13ebf9e663b70a3aba
jazzthedog <jazzthedog.bowwow@gmail.com>
2015-08-04 19:51:03 +0000

    added protection at the controller level for creating new ingredients and styles

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -68,7 +68,8 @@ class RecipesController  < ApplicationController
     def require_same_user
     	if current_user != @recipe.chef
     		flash[:danger] = "You can only edit your own recipe"
-    		redirect_to = recipes_path
+    		redirect_to recipes_path
     	end
     end    
+    
 end
